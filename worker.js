7f81bc56f8226cdf7b686c47bde3d850c2e7fcd6
John Muthua <caveomuthama@gmail.com>
2024-12-11 22:48:50 +0300

    Update worker.js

--- worker.js
+++ worker.js
@@ -62,14 +62,14 @@ userQueue.process(async (job, done) => {
   }
   console.log(`Welcome ${user.email}!`);
   try {
-    const mailSubject = 'Welcome to ALX-Files_Manager by B3zaleel';
+    const mailSubject = 'Welcome to ALX-Files_Manager by Cavein254';
     const mailContent = [
       '<div>',
       '<h3>Hello {{user.name}},</h3>',
       'Welcome to <a href="https://github.com/B3zaleel/alx-files_manager">',
       'ALX-Files_Manager</a>, ',
       'a simple file management API built with Node.js by ',
-      '<a href="https://github.com/B3zaleel">Bezaleel Olakunori</a>. ',
+      '<a href="https://github.com/Cavein254">John Muthua</a>. ',
       'We hope it meets your needs.',
       '</div>',
     ].join('');
